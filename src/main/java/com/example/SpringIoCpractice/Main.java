448c2b154b7fcfd1fed64afaef69d25b593725ee
linyues <2694166721@qq.com>
2021-10-13 22:32:20 +0800

    I dont know what went wrong

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,20 @@
-//TODO
+package com.example.SpringIoCpractice;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //1、创建Spring的IOC容器对象
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("hello.xml");
+
+        //2、从IOC容器中获取Bean实例
+        HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+
+        //3、调用hello方法
+        helloWorld.getHello();
+
+    }
+}
